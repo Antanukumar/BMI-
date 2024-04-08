07c481b2ecf39891fce707898c63ff92da261855
Antanukumar <antanukumar9@gmail.com>
2024-04-08 13:46:17 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello!');
+// console.log('Hello!');
+
+
+const form= document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const height=parseInt(document.querySelector("#height").value)
+
+    const weight=parseInt(document.querySelector("#weight").value)
+
+    const result=parseInt(document.querySelector("#result"))
+
+    if(height === ''||height <0||isNaN(height)){
+        result.innerHTML =`please give a valid height ${height}`;
+    }
+
+    else if(weight === ''||weight <0||isNaN(weight)){
+        result.innerHTML =`please give a valid weight ${weight}`;
+    }
+    else{
+        const BMI=(weight /(height *height)/10000).toFixed(2)
+
+        result.innerHTML= `<span> ${BMI}</span>`
+    }
+
+
+    
+})
